393d61994e31d3e32cea417126784e1e4dec499a
matthewhopwood <matthewhopwood21@hotmail.com>
2019-10-08 12:49:26 +0100

    Initial commit

---
+++ lib/ddarkbluu_view_tool.rb
@@ -0,0 +1,7 @@
+require "ddarkbluu_view_tool/version"
+require "ddarkbluu_view_tool/renderer"
+
+module DdarkbluuViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ddarkbluu_view_tool/version.rb
@@ -0,0 +1,3 @@
+module DdarkbluuViewTool
+  VERSION = "0.1.0"
+end
